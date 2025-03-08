f64ee2e315382572d027e98cb961c461520ed8f8
leonardcser <73912641+leonardcser@users.noreply.github.com>
2025-03-08 09:53:24 +0100

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -33,6 +33,7 @@ export default ts.config(
   },
   {
     rules: {
+      'svelte/require-each-key': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/ban-ts-comment': 'off',
       '@typescript-eslint/no-unused-vars': [

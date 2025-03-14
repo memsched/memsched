550e0e6256294a051184875d89c01546d79be448
leonardcser <73912641+leonardcser@users.noreply.github.com>
2025-03-14 14:25:18 +0100

    fix: make env vars dynamic

--- src/lib/server/oauth.ts
+++ src/lib/server/oauth.ts
@@ -1,22 +1,17 @@
 import { GitHub, Google } from 'arctic';
 import { PUBLIC_GITHUB_REDIRECT_URI, PUBLIC_GOOGLE_REDIRECT_URI } from '$env/static/public';
-import {
-  GITHUB_CLIENT_ID,
-  GITHUB_CLIENT_SECRET,
-  GOOGLE_CLIENT_ID,
-  GOOGLE_CLIENT_SECRET,
-} from '$env/static/private';
+import { env } from '$env/dynamic/private';
 
 export type ProviderId = 'github' | 'google';
 
 export const github = new GitHub(
-  GITHUB_CLIENT_ID,
-  GITHUB_CLIENT_SECRET,
+  env.GITHUB_CLIENT_ID,
+  env.GITHUB_CLIENT_SECRET,
   PUBLIC_GITHUB_REDIRECT_URI
 );
 
 export const google = new Google(
-  GOOGLE_CLIENT_ID,
-  GOOGLE_CLIENT_SECRET,
+  env.GOOGLE_CLIENT_ID,
+  env.GOOGLE_CLIENT_SECRET,
   PUBLIC_GOOGLE_REDIRECT_URI
 );

16e982d3d44ba58f8155caaf1485067832e1f44b
leonardcser <73912641+leonardcser@users.noreply.github.com>
2025-03-19 19:57:14 +0100

    feat: improve profile page when no objectives

--- src/routes/(home)/[username]/+page.svelte
+++ src/routes/(home)/[username]/+page.svelte
@@ -42,34 +42,38 @@
     edit={data.isOwner}
   />
   <div class="flex w-full flex-col gap-8">
-    {#if data.publicObjectives.length > 0}
-      <div class="w-full space-y-3">
-        <h2 class="h3">Widgets</h2>
-        {#if data.publicUser.widgets.length > 0}
-          <div class="flex flex-wrap gap-3">
-            {#each data.publicUser.widgets as widget}
-              <a href="/widgets/{widget}">
-                <img
-                  src="/api/widgets/{widget}?svg&v={uuid4()}"
-                  alt="MEMsched Widget"
-                  class="h-[85px]"
-                />
-              </a>
-            {/each}
-          </div>
-        {:else}
-          <div
-            class="flex h-48 w-full flex-col items-center justify-center gap-3 rounded-lg border bg-muted p-4 text-muted-foreground"
-          >
-            No public widgets yet
-            {#if data.isOwner}
+    <div class="w-full space-y-3">
+      <h2 class="h3">Widgets</h2>
+      {#if data.publicUser.widgets.length > 0}
+        <div class="flex flex-wrap gap-3">
+          {#each data.publicUser.widgets as widget}
+            <a href="/widgets/{widget}">
+              <img
+                src="/api/widgets/{widget}?svg&v={uuid4()}"
+                alt="MEMsched Widget"
+                class="h-[85px]"
+              />
+            </a>
+          {/each}
+        </div>
+      {:else}
+        <div
+          class="flex h-48 w-full flex-col items-center justify-center gap-3 rounded-lg border bg-muted p-4 text-muted-foreground"
+        >
+          No public widgets yet
+          {#if data.isOwner}
+            {#if data.publicObjectives.length > 0}
               <IconButton href="/widgets/new" size="sm" icon={IoAdd}
                 >Create your first widget</IconButton
               >
+            {:else}
+              <IconButton href="/objectives/new" size="sm" icon={IoAdd}
+                >Create your first objective</IconButton
+              >
             {/if}
-          </div>
-        {/if}
-      </div>
-    {/if}
+          {/if}
+        </div>
+      {/if}
+    </div>
   </div>
 </section>

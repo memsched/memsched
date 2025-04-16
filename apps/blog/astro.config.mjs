dd5acdc19ad9bcb8eb74a20ba7d743f701d6b769
leonardcser <73912641+leonardcser@users.noreply.github.com>
2025-04-16 16:14:16 +0200

    style: format

--- apps/blog/astro.config.mjs
+++ apps/blog/astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
-import svelte from "@astrojs/svelte";
-import mdx from "@astrojs/mdx";
+import tailwind from '@astrojs/tailwind';
+import svelte from '@astrojs/svelte';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://blog.memsched.com",
+  site: 'https://blog.memsched.com',
   integrations: [tailwind(), svelte(), mdx()],
 });

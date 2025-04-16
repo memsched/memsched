9401a7d034891c8bd2bed82b138687ee9eb17613
leonardcser <73912641+leonardcser@users.noreply.github.com>
2025-04-16 12:29:22 +0200

    feat: add astro blog svelte support

--- apps/blog/astro.config.mjs
+++ apps/blog/astro.config.mjs
@@ -3,10 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 import tailwind from '@astrojs/tailwind';
-  
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), svelte()],
   adapter: cloudflare({
     platformProxy: {
       enabled: true

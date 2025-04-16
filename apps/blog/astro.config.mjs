429c5ed7ae91cda79fa6695c1b041ec1d7b2804d
leonardcser <73912641+leonardcser@users.noreply.github.com>
2025-04-16 14:12:28 +0200

    feat: add blog layout

--- apps/blog/astro.config.mjs
+++ apps/blog/astro.config.mjs
@@ -4,10 +4,12 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import tailwind from '@astrojs/tailwind';
 import svelte from '@astrojs/svelte';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  site: 'https://blog.memsched.com',
+  integrations: [tailwind(), svelte(), mdx()],
   adapter: cloudflare({
     platformProxy: {
       enabled: true
---
+++ apps/blog/src/lib/constants.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = 'Memsched Blog';
+export const SITE_DESCRIPTION = 'Memsched Blog';
